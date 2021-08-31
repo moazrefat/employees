d3bb6d8f279a41c48ac944b20a2431b25d46b901
Moaaz Noaman <moaz.refat@hotmail.com>
2021-08-31 22:11:01 +0200

    initial commit

--- version/app.py
+++ version/app.py
@@ -31,3 +31,10 @@ def health():
         version=version,
         status=status
     )
+
+if __name__ == '__main__':
+    app.debug = True
+    host = os.environ.get('IP', '0.0.0.0')
+    # print(host)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host=host, port=port)
