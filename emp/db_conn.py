835f798f1be451f25f60db06739912071acce9ea
Son of Denethor II <boromir@pharos.altara.zitis.lan>
2022-02-10 19:22:29 +0100

    commit

--- emp/db_conn.py
+++ emp/db_conn.py
@@ -8,7 +8,7 @@ DB_LB = os.environ['MYSQL_LB']
 # DB_PASSWORD = os.environ['MYSQL_PASSWORD']
 # DB_LB='localhost'
 MYSQL_ROOT_PASSWORD='dontplaywithme'
-DB_PORT=80
+# DB_PORT=80
 DB_NAME='app'
 DB_USER='root'
 DB_PASSWORD='dontplaywithme'
@@ -20,7 +20,6 @@ class db():
     def __enter__(self):
         try:
             self.mysq = mysql.connector.connect(host=DB_LB,
-                                                port=DB_PORT,
                                                 user=DB_USER,
                                                 database=DB_NAME,
                                                 passwd=DB_PASSWORD)
